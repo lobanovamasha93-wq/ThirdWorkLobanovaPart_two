1ac01b4584824583c60d979dff61ee8eb25fbd13
Maria Lobanova <marusa.lobanova2015@yandex.ru>
2026-01-16 14:39:56 +0300

    Add Service Bmi

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        BmiService service = new BmiService();
+
+        double weight = 59.0;
+        double height = 1.55;
+
+        int bmi = service.calculate(weight, height);
+
+        System.out.println("Вес: " + weight + " кг");
+        System.out.println("Рост: " + height + " м");
+        System.out.println("Индекс массы тела (BMI): " + bmi);
     }
 }
